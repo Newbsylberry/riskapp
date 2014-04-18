48fb72ae90c0180ac8f907ed132b92e441719cfd
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-04-17 21:09:29 -0400

    Added pie chart for exposure

--- app/assets/javascripts/angular/controllers/portfolio_home_ctrl.js
+++ app/assets/javascripts/angular/controllers/portfolio_home_ctrl.js
@@ -7,11 +7,19 @@ app.controller('PortfolioHomeCtrl', ['$scope', '$routeParams', 'Portfolio',
         // addRiskToChart pushes the risks from the Portfolio.get into chartConfig.series.data
         //  so that it can be displayed in html
         var addProjectToChart = function(project) {
-            $scope.chartConfig.series[0].data.push([project.name, parseFloat(project.total_exposure)]);
+            $scope.pieChartConfig.series[0].data.push([project.name, parseFloat(project.total_exposure)]);
         };
+
+
         Portfolio.get({portfolioId: $routeParams.portfolioId}, function(successResponse) {
             $scope.portfolio = successResponse;
-            $scope.chartConfig.title.text = successResponse.name;
+            $scope.pieChartConfig.title.text = successResponse.name;
+            $scope.barChartConfig.series[0].data.push(['Short Term Exposure',
+                parseFloat(successResponse.short_term_exposure)]);
+            $scope.barChartConfig.series[0].data.push(['Mid Term Exposure',
+                parseFloat(successResponse.mid_term_exposure)]);
+            $scope.barChartConfig.series[0].data.push(['Long Term Exposure',
+                parseFloat(successResponse.long_term_exposure)]);
             console.log("success response " + successResponse );
             console.log(successResponse);
             angular.forEach(successResponse.projects, addProjectToChart);
@@ -19,7 +27,7 @@ app.controller('PortfolioHomeCtrl', ['$scope', '$routeParams', 'Portfolio',
             console.log("error response");
             console.log(errorResponse);
         });
-        $scope.chartConfig = {
+        $scope.pieChartConfig = {
             options: {
                 chart: {
                     type: 'pie'
@@ -30,6 +38,20 @@ app.controller('PortfolioHomeCtrl', ['$scope', '$routeParams', 'Portfolio',
             }],
             title: {
                 text: 'Loading...'
+            },
+            loading: false
+        }
+        $scope.barChartConfig = {
+            options: {
+                chart: {
+                    type: 'pie'
+                }
+            },
+            series: [{
+                data: []
+            }],
+            title: {
+                text: 'Portfolio Exposure by Timeline'
             },
             loading: false
         }
