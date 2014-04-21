c01975e19cefb9e3cb3983a6da389709187ad16e
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-04-21 02:05:21 -0400

    Added semicolons

--- app/assets/javascripts/angular/controllers/risks_ctrl.js
+++ app/assets/javascripts/angular/controllers/risks_ctrl.js
@@ -10,10 +10,10 @@ app.controller('RisksCtrl', ['$scope', 'Risk', function($scope, Risk) {
         attr.impact_rating = ($scope.newRisk.impact_rating);
         attr.probability = ($scope.newRisk.probability / 100 );
         attr.schedule_impact = ($scope.newRisk.schedule_impact);
-        attr.event_id = ($scope.newRisk.event_id)
-        attr.risk_type_id = ($scope.newRisk.risk_type_id)
-        attr.risk_status_id = ($scope.newRisk.risk_status_id)
-        attr.risk_control_category_id = ($scope.newRisk.risk_control_category_id)
+        attr.event_id = ($scope.newRisk.event_id);
+        attr.risk_type_id = ($scope.newRisk.risk_type_id);
+        attr.risk_status_id = ($scope.newRisk.risk_status_id);
+        attr.risk_control_category_id = ($scope.newRisk.risk_control_category_id);
         attr.project_id = ($scope.newRisk.project_id);
         attr.critical = ($scope.newRisk.critical);
         var newRisk = Risk.create(attr);
